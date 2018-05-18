1af121e3bdc0b1dd372c5a5126cbbe0708faf3c0
DreamerOfKnowledge <felikssakovych@gmail.com>
2018-05-18 16:16:27 +0300

    ver 7

--- csstasks/test/jquery-samples/combobox-widget.js
+++ csstasks/test/jquery-samples/combobox-widget.js
@@ -1,11 +1,10 @@
 (function ($) {
     $.fn.displayCombo = function(options) {
-        $.fn.displayCombo.defaults = $.extend({}, $.fn.displayCombo.defaults, options);
-
         var $holder = $(this);
 
-        $(this).each(function () {
-            $container = $(this);
+        return this.each(function () {
+            $.fn.displayCombo.defaults = $.extend({}, $.fn.displayCombo.defaults, options);
+            var $container = $(this);
             $container.on("selectedItem:toggle", changeItem);
 
             setContentStyles(createContent($container));
@@ -15,114 +14,118 @@
             var value = $container.data().value;
             var listClass = '.' + $.fn.displayCombo.defaults.dropdownList;
             var displayTextClass = '.' + $.fn.displayCombo.defaults.displayText;
+            changeText($container);
 
-            $container.find(listClass).children('div').each(function () {
-                var data = $(this).data().value;
-                if(validateData(data) && validateData(value) && data === value){
-                    $container.find(displayTextClass).text(this.textContent);
-                    $(this).addClass("selected");
+            $(document).on("click", function(event){
+                if(event.target.id !== $.fn.displayCombo.defaults.buttonId || 
+                    event.target.id !== $.fn.displayCombo.defaults.iconId){
+                    $('.' + $.fn.displayCombo.defaults.dropdownList).css("display", "none");
                 }
             });
-        });
-
-        $(document).on("click", function(event){
-            if(event.target.id !== $.fn.displayCombo.defaults.buttonId || 
-                event.target.id !== $.fn.displayCombo.defaults.iconId){
-                $('.' + $.fn.displayCombo.defaults.dropdownList).css("display", "none");
+    
+            function changeText(container){
+                var data1 = container.data().value;
+                container.find('.' + $.fn.displayCombo.defaults.dropdownList).children('div').each(function () {
+                    $elem = $(this);
+                    var data2 = $elem.data().value;
+                    if(validateData(data1) && validateData(data2) && data1 == data2){
+                        container.find('.' + $.fn.displayCombo.defaults.displayText).text(this.textContent);
+                        $elem.addClass("selected");
+                    }
+                });
             }
+    
+            function validateData(value){
+                return typeof value !== 'undefined';
+            }
+    
+            function changeItem(event, data) {
+                var $container = $(this);
+                $container.data().value = data;
+                changeText($container);
+            }
+    
+            function createContent(container){
+                var content = $('<div/>', {
+                    class: $.fn.displayCombo.defaults.dropdownList,
+                    click: function (event){
+                        container.trigger("selectedItem:toggle",  event.target.attributes[0].value);
+                        $(this).find('.selected').removeClass();
+                        $(event.target).addClass("selected"); 
+                        $(this).css("display", "none");
+                    }
+                });
+        
+                container.children('div').appendTo(content);
+                container.append(content);
+        
+                return content;
+            }
+        
+            function createArrow() {
+                return $('<i/>', { 
+                    class: $.fn.displayCombo.defaults.icon,
+                    id: $.fn.displayCombo.defaults.iconId 
+                });
+            }
+        
+            function createTextElement(container) {
+                var text = $('<p/>', { class: $.fn.displayCombo.defaults.displayText });
+                container.append(text);
+        
+                return text;
+            }
+        
+            function createButton(container){
+                var arrow = createArrow();
+                var button = $('<div/>', {
+                    class: $.fn.displayCombo.defaults.dropdownButton,
+                    id: $.fn.displayCombo.defaults.buttonId,
+                    click: function (event){
+                        var listClass = '.' + $.fn.displayCombo.defaults.dropdownList;
+                        var dropDownContent = container.find(listClass);
+                        $holder.each(function(){
+                            $(this).find(listClass).hide();
+                        });
+                        dropDownContent.is(':visible') ? dropDownContent.hide() : dropDownContent.show();
+                        return false;
+                    }
+                }).append(arrow);
+        
+                container.append(button);
+                return button;
+            }
+        
+            function setContentStyles(content){
+                content.css({
+                    "display" : "none",
+                    "position" : "absolute",
+                    "top" : $container.height() + "px",
+                    "min-width" : "200px"
+                });
+            }
+        
+            function setTextStyles(pElem){
+                pElem.css({
+                    "margin" : "0",
+                    "padding" : "10px",
+                    "float": "left",
+                    "font-size": "16px"
+                });
+            }
+        
+            function setButtonStyles(btn){
+               btn.css({
+                    "border-left" : "1px solid black",
+                    "float" : "right",
+                    "width" : "15%",
+                    "height" : "100%",
+                    "background-color" : "lightgray"
+                });
+            }
+
+            return $container;
         });
-
-        function validateData(value){
-            return typeof value !== 'undefined';
-        }
-
-        function changeItem(event, data) {
-            $(this).find('.'+$.fn.displayCombo.defaults.displayText).text(data);
-        }
-
-        function dropDownContentClick(event){
-            $(this).parent().trigger("selectedItem:toggle",  event.target.textContent);
-            $(this).find('.selected').removeClass();
-            $(event.target).addClass("selected"); 
-            $(this).css("display", "none");
-        };
-
-        function showOptionList(event){
-            var listClass = '.' + $.fn.displayCombo.defaults.dropdownList;
-            var dropDownContent = $(this).parent().find(listClass);
-            $holder.each(function(){
-                $(this).find(listClass).hide();
-            });
-            dropDownContent.is(':visible') ? dropDownContent.hide() : dropDownContent.show();
-            return false;
-        }
-
-        function createContent(container){
-            var content = $('<div/>', {
-                class: $.fn.displayCombo.defaults.dropdownList,
-                click: dropDownContentClick
-            });
-    
-            container.children('div').appendTo(content);
-            container.append(content);
-    
-            return content;
-        }
-    
-        function createArrow() {
-            return $('<i/>', { 
-                class: $.fn.displayCombo.defaults.icon,
-                id: $.fn.displayCombo.defaults.iconId 
-            });
-        }
-    
-        function createTextElement(container) {
-            var text = $('<p/>', { class: $.fn.displayCombo.defaults.displayText });
-            container.append(text);
-    
-            return text;
-        }
-    
-        function createButton(container){
-            var arrow = createArrow();
-    
-            var button = $('<div/>', {
-                class: $.fn.displayCombo.defaults.dropdownButton,
-                id: $.fn.displayCombo.defaults.buttonId,
-                click: showOptionList
-            }).append(arrow);
-    
-            container.append(button);
-            return button;
-        }
-    
-        function setContentStyles(content){
-            content.css({
-                "display" : "none",
-                "position" : "absolute",
-                "top" : $container.height() + "px",
-                "min-width" : "200px"
-            });
-        }
-    
-        function setTextStyles(pElem){
-            pElem.css({
-                "margin" : "0",
-                "padding" : "10px",
-                "float": "left",
-                "font-size": "16px"
-            });
-        }
-    
-        function setButtonStyles(btn){
-           btn.css({
-                "border-left" : "1px solid black",
-                "float" : "right",
-                "width" : "15%",
-                "height" : "100%",
-                "background-color" : "lightgray"
-            });
-        }
     };
 
     $.fn.displayCombo.defaults = {
@@ -131,6 +134,7 @@
         displayText : 'text',
         icon : 'fas fa-caret-down',
         iconId : 'iconId',
-        buttonId : 'buttonId'
+        buttonId : 'buttonId',
+        $container : null
     }; 
 }(jQuery));
