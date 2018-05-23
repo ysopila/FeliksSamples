6140eeea0d57574deef6664ca2428bd52f293026
DreamerOfKnowledge <felikssakovych@gmail.com>
2018-05-23 12:40:10 +0300

    ver 2

--- csstasks/test/jquery-samples/file-uploader.js
+++ csstasks/test/jquery-samples/file-uploader.js
@@ -2,16 +2,17 @@
     $.fn.displayImageCtrl = function(options) {
         return this.each(function() {
             var settings = $.extend({}, $.fn.displayImageCtrl.defaults, options);
-            var $container = $(this);
 
+            var $container = createMainContainer($(this));
             createButton($container);
 
-            function createZoomedImage(container, src){
+            function createZoomedImage(container, originalImage) {
                 return $('<img/>', {
                     class: settings.zoomedImageClass
-                }).attr("src", src).css({
-                    "transform" : "scale(3)",
-                    "position" : "absolute"
+                }).attr("src", originalImage.attr("src")).css({
+                    "width" : originalImage.width() * settings.scaleFactor,
+                    "height" : originalImage.height() * settings.scaleFactor,
+                    "position": "absolute"
                 }).appendTo(container);
             }
 
@@ -26,9 +27,10 @@
                         $container.find('.' + settings.zoomedPopupClass).remove();
                     },
                     mousemove: function(event) {
-                        moveZoomScreen(event.pageX, event.pageY);
+                        var offset = $(event.target).offset();
+                        moveZoomScreen(event.pageX - offset.left, event.pageY - offset.top);
                     }
-                }).appendTo(container).css({
+                }).appendTo($container).css({
                     "left": "0",
                     "top": "0",
                     "position": "absolute",
@@ -36,7 +38,7 @@
                     "height": height
                 });
             }
-            
+
             function createZoomPopup(container) {
                 return $('<div/>', {
                     class: settings.zoomedPopupClass
@@ -44,12 +46,30 @@
                     "background-color": "white",
                     "top": "0",
                     "left": "0",
-                    "padding" : "0",
-                    "position": "relative",
+                    "padding": "0",
+                    "position": "absolute",
                     "width": settings.widthZoomWindow,
                     "height": settings.heightZoomWindow,
                     "border": "1px solid black",
-                    "overflow" : "hidden"
+                    "overflow": "hidden"
+                }).appendTo(container);
+            }
+
+            function createMainContainer(container) {
+                return $('<div/>', {
+                    class: settings.mainClass
+                }).css({
+                    "background-color": "lightgrey",
+                    "margin": "0 auto",
+                    "position": "relative",
+                    "border": "2px solid black",
+                    "width": "60%",
+                    "height": "500px",
+                    "margin-top": "200px"
+                    /*                     "top": "50%",
+                                        "left": "50%",
+                                        "position": "relative",
+                                        "transform": "translate(-50%, -50%)", */
                 }).appendTo(container);
             }
 
@@ -57,14 +77,12 @@
                 return $('<img/>', {
                     class: settings.imageHolderClass,
                     mouseenter: function(event) {
-                        createOverlayContainer(container,  $(event.target).width(), $(event.target).height());
-                        createZoomedImage(createZoomPopup(container), $(event.target).attr("src"));
+                        createOverlayContainer(container, $(event.target).width(), $(event.target).height());
+                        createZoomedImage(createZoomPopup(container), $(event.target));
                     }
                 }).css({
-                    "position": "absolute",
-                    "top": "0",
-                    "left": "0",
-                    "margin" : "0 auto"
+                    "max-width": "100%",
+                    "max-height": "100%"
                 }).appendTo(container);
             }
 
@@ -77,6 +95,7 @@
                             var reader = new FileReader();
                             reader.onload = function(e) {
                                 createImageHolder(container).attr('src', reader.result);
+                                $('.' + settings.imageButtonClass).remove();
                             };
                             reader.readAsDataURL(this.files[0]);
                         }
@@ -90,34 +109,23 @@
                 }).appendTo(container);
             }
 
-            //function validateYPosition(y){
-                //return (y > 75 && y < ($.fn.displayImageCtrl.defaults.height - 75));
-            //}
-
-            //function validateXPosition(x){
-               // return (x > 50 && x < ($.fn.displayImageCtrl.defaults.width - 50));
-            //}
-
             function moveZoomScreen(x, y) {
-                //var obj = $('.' + settings.imageHolderClass).getBoundingClientRect();
-                $('.' + settings.zoomedImageClass).css({ "top": (-(y) + 50) + "px ", "left" : (-(x) + 50) + "px" });
+                $('.' + settings.zoomedImageClass).css({ 
+                    "top": ((-y * settings.scaleFactor) + 75) + "px ", 
+                    "left": ((-x * settings.scaleFactor) + 75) + "px" });
 
-                var pos = { "top": $('.' + settings.zoomedPopupClass).css("top"), "left": $('.' + settings.zoomedPopupClass).css("left") };
+                $('.' + settings.zoomedPopupClass).css({ 
+                    "top": (y - 75) + "px", 
+                    "left": (x + 75) + "px" 
+                });
 
-                //if(validateXPosition(x)){
-                    pos.left = (x + 75) + "px";
-                //}
-                //if(validateXPosition(y)){
-                    pos.top = (y - 120) + "px";
-                //} 
-
-                $('.' + settings.zoomedPopupClass).css(pos);
+                var size = (settings.sizeSquare/2);
 
                 $('.' + settings.overlayClass).css({
-                    "border-bottom": $.fn.displayImageCtrl.defaults.height - (y + 50) + "px solid rgba(193, 193, 193, .5)",
-                    "border-top": (y - 50) + "px solid rgba(193, 193, 193, .5)",
-                    "border-left": (x - 50) + "px solid rgba(193, 193, 193, .5)",
-                    "border-right": $.fn.displayImageCtrl.defaults.width - (x + 50) + "px solid rgba(193, 193, 193, .5)"
+                    "border-bottom": $.fn.displayImageCtrl.defaults.height - (y + size) + "px solid rgba(193, 193, 193, .5)",
+                    "border-top": (y - size) + "px solid rgba(193, 193, 193, .5)",
+                    "border-left": (x - size) + "px solid rgba(193, 193, 193, .5)",
+                    "border-right": $.fn.displayImageCtrl.defaults.width - (x + size) + "px solid rgba(193, 193, 193, .5)"
                 });
             }
         });
@@ -127,7 +135,7 @@
 
     $.fn.displayImageCtrl.defaults = {
         heightZoomWindow: "150px",
-        widthZoomWindow: "200px",
+        widthZoomWindow: "150px",
         xPosZoomWindow: 75,
         yPosZoomWindow: 120,
         overlayClass: 'overlay',
@@ -135,10 +143,11 @@
         imageHolderClass: 'img-holder',
         imageButtonClass: 'load-image-button',
         zoomedImageClass: 'zoomed-image',
-
+        mainClass: 'main',
         imageWidth: null,
         imageHeight: null,
-
+        sizeSquare: 100,
+        scaleFactor: 3,
         textButton: "Load Image"
     };
 }(jQuery));
